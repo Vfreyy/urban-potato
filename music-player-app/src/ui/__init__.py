603e584ddc25a7f22823a68396b61537a63c2f5f
Vfreyy <buenastardesuwur8268uwu@gmail.com>
2025-09-26 16:22:16 -0400

    Add files via upload

---
+++ music-player-app/src/ui/__init__.py
@@ -0,0 +1,12 @@
+class UserInterface:
+    def __init__(self):
+        pass
+
+    def display_track_list(self, tracks):
+        pass
+
+    def display_playback_controls(self):
+        pass
+
+    def update_playback_status(self, status):
+        pass
